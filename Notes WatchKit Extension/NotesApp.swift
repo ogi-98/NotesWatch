493226e89fb1aaed63b589906ef0a56821f5b09e
ogi-98 <oguzkaya99@hotmail.com>
2021-10-04 14:37:19 +0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Oğuz Kaya on 4.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
