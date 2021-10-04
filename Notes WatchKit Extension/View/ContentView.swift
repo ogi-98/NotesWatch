868391ea10b6792ef4f5cc7bc558a082ddfd73c1
ogi-98 <oguzkaya99@hotmail.com>
2021-10-04 18:09:39 +0300

    Store data, Read data and ListView

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -14,8 +14,46 @@ struct ContentView: View {
     
     
     //MARK: - FUNCS
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     func saveNotes() {
-        dump(notes)
+//        dump(notes)
+        DispatchQueue.main.async {
+            do {
+                // 1. convert the otes array to data with JSONEncoder
+                let data = try JSONEncoder().encode(notes)
+                // 2. create a new URL to save the file using getDocumentDirectory
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                // 3. write the data to given URL
+                try data.write(to: url)
+            } catch  {
+                print("saving data is faild")
+            }
+        }
+    }
+    
+    func loadNotes() {
+        DispatchQueue.main.async {
+            do {
+                // 1. Get the notes URL path
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                // 2. Create a new property for the data
+                let data = try Data(contentsOf: url)
+                // 3. Decode the data
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+            
+            }
+        }
+    }
+    func deleteNote(offSets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offSets)
+            saveNotes()
+        }
     }
     
     //MARK: - BODY
@@ -50,9 +88,39 @@ struct ContentView: View {
             }//: HStack
             Spacer()
             
-            Text("\(notes.count)")
+            if notes.count >= 1 {
+                List {
+                    ForEach(0..<notes.count, id: \.self) { i in
+                        HStack {
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading, 5)
+                        }//: HStack
+                    }//: Loop
+                    .onDelete { indexNum in
+                        deleteNote(offSets: indexNum)
+                    }
+                }
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(20)
+                Spacer()
+            }//: List
         }//: VStack
         .navigationTitle("Notes")
+//        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            loadNotes()
+        }
         
     }
 }
